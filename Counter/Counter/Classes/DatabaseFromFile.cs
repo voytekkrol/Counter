cf16e3b247b84e7b55f4f9731f45c0ff0d05c774
voytekkrol <wojtekmkrol@gmail.com>
2020-07-18 11:04:01 +0200

    DatabaseFromFile created

---
+++ Counter/Counter/Classes/DatabaseFromFile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter.Classes
+{
+    public class DatabaseFromFile
+    {
+        public static IEnumerable<Hour> Read()
+        {
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
+            {
+                IEnumerable<Hour> tempListOfHours;
+                conn.CreateTable<Hour>();
+                return tempListOfHours = conn.Table<Hour>().ToList();
+            }
+        }
+
+        public static IEnumerable<Hour> AddHour()
+        {
+            Hour hour = new Hour();
+            hour.dateTime = DateTime.Today;
+            hour.anHour = 1;
+
+            using (SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
+            {
+                IEnumerable<Hour> tempListOfHours;
+                connection.CreateTable<Hour>();
+                connection.Insert(hour);
+                return tempListOfHours = connection.Table<Hour>().ToList();
+
+            }
+        }
+
+        public static IEnumerable<Hour> DeleteHour()
+        {
+            using (SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
+            {
+                IEnumerable<Hour> tempListOfHours;
+                connection.CreateTable<Hour>();
+                Hour hour = connection.Table<Hour>().Last<Hour>();
+                connection.Delete(hour);
+                return tempListOfHours = connection.Table<Hour>().ToList();
+            }
+        }
+    }
+}
--- Counter/Counter/MainWindow.xaml.cs
+++ Counter/Counter/MainWindow.xaml.cs
@@ -1,47 +1,28 @@
 ﻿using Counter.Classes;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Counter
 {
     public partial class MainWindow : Window
     {
-        List<Hour> listOfHours;
+        IEnumerable<Hour> listOfHours;
         public double AverageHours { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
-            listOfHours = new List<Hour>();
-            ReadDatabase();
+            listOfHours = DatabaseFromFile.Read();
             CounterRefresh();
         }
 
-        void ReadDatabase()
-        {
-            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
-            {
-                conn.CreateTable<Hour>();
-                listOfHours = conn.Table<Hour>().ToList();
-            }
-        }
         private void CounterRefresh()
         {
            
-            Counter.Text = $"Average: {CountAverageHours().ToString()} hours";
+            Counter.Text = $"Average: {CountAverageHours()} hours";
         }
 
         private void Exit_Clicked(object sender, RoutedEventArgs e)
@@ -50,29 +31,13 @@ namespace Counter
         }
         private void AddButton_Clicked(object sender, RoutedEventArgs e)
         {
-            Hour hour = new Hour();
-            hour.dateTime = DateTime.Today;
-            hour.anHour = 1;
-
-            using(SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
-            {
-                connection.CreateTable<Hour>();
-                connection.Insert(hour);
-                listOfHours = connection.Table<Hour>().ToList();
-                
-            }
+            listOfHours = DatabaseFromFile.AddHour();
             CounterRefresh();
         }
 
         private void SubtractButton_Clicked(object sender, RoutedEventArgs e)
         {
-            using (SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
-            {
-                connection.CreateTable<Hour>();
-                Hour hour = connection.Table<Hour>().Last<Hour>();
-                connection.Delete(hour);
-                listOfHours = connection.Table<Hour>().ToList();
-            }
+            listOfHours = DatabaseFromFile.DeleteHour();
             CounterRefresh();
         }
 
@@ -90,7 +55,7 @@ namespace Counter
 
         private double CountAverageHours()
         {
-            return AverageHours = Math.Round((listOfHours.Count / DaysFromStart()), 2);
+            return AverageHours = Math.Round(listOfHours.Count() / DaysFromStart(), 2);
         }
 
 
