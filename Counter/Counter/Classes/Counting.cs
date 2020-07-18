f1413da89f25008f6f331d18eaa268249eefd894
voytekkrol <wojtekmkrol@gmail.com>
2020-07-18 14:07:34 +0200

    Cosmetic changes

--- Counter/Counter/Classes/Counting.cs
+++ Counter/Counter/Classes/Counting.cs
@@ -11,14 +11,14 @@ namespace Counter.Classes
         
         public static double DaysFromStart(IEnumerable<Hour> listOfHours)
         {
-            DateTime firstDay = listOfHours.ElementAt<Hour>(0).dateTime;
+            var firstDay = listOfHours.ElementAt<Hour>(0).dateTime;
             return DateTime.Today.Subtract(firstDay).TotalDays;
         }
 
         public static double CountAverageHours(IEnumerable<Hour> listOfHours, Func<IEnumerable<Hour>, double> daysFromStart)
         {
             var tempValue =  Double.Parse(daysFromStart.ToString());
-            return  Math.Round((double)listOfHours.Count() / tempValue, 2);
+            return  Math.Round(listOfHours.Count() / tempValue, 2);
         }
     }
 }
